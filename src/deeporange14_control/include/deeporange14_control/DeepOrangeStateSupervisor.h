f466a07c269f4fa133438aa152709fc2ef07aab2
Sanskruti Jadhav <64002247+Autonomousanz@users.noreply.github.com>
2023-07-25 10:27:56 -0400

    Update DeepOrangeStateSupervisor.h
    Added comments

--- src/deeporange14_control/include/deeporange14_control/DeepOrangeStateSupervisor.h
+++ src/deeporange14_control/include/deeporange14_control/DeepOrangeStateSupervisor.h
@@ -34,50 +34,38 @@ namespace deeporange14
         ~DeepOrangeStateSupervisor();
 
         private:
-        void checkStackStatus(const geometry_msgs::Twist::ConstPtr& cmdVelMsg);
-        
-        void getMissionStatus(const std_msgs::String::ConstPtr& missionStatus);
-    
-        
-        void getTorqueValues(const deeporange14_msgs::TorqueCmdStamped::ConstPtr& controllerTrqValues);
-
-        void getStopRos(const std_msgs::Bool::ConstPtr& stopRosMsg);
-
-        void getRaptorMsg(const deeporange14_msgs::RaptorStateMsg::ConstPtr& raptorMsg);
-
-        void supervisorControlUpdate(const ros::TimerEvent& event);
-
+        void checkStackStatus(const geometry_msgs::Twist::ConstPtr& cmdVelMsg); // Stack fault is  true if there are no new cmd_vel msgs from stack side
+        void getMissionStatus(const std_msgs::String::ConstPtr& missionStatus); //get stack status and set to class variables      
+        void getTorqueValues(const deeporange14_msgs::TorqueCmdStamped::ConstPtr& controllerTrqValues);// Get torque values from velocity controller node
+        void getStopRos(const std_msgs::Bool::ConstPtr& stopRosMsg);// get bool of stopRos from stack side
+        void getRaptorMsg(const deeporange14_msgs::RaptorStateMsg::ConstPtr& raptorMsg); // get raptor systems, dbwmode, speed st, and raptor hb timeout check
+        void supervisorControlUpdate(const ros::TimerEvent& event); // contantly publish appropriate states based on logic and transition at 50Hz
         void updateROSStateMsg();
 
         //member variables 
-        bool raptor_hb_detected;
-        bool stack_fault;
-    
-        bool dbw_ros_mode;
-        std::string mission_status;
-        float brkL_pr;
-        float brkR_pr;
-        float tqL_cmd_controller;
-        float tqR_cmd_controller;
-        bool stop_ros;
-        bool raptorbrakeAck;
-        uint prevSt;
-        uint delay;
-
-        allStates state;
-        double raptor_hb_timestamp;
-        double cmdvel_timestamp;
-        uint speed_state;
-        uint au_state;
+        bool raptor_hb_detected; // true if raptor timestamp and msg are old
+        bool stack_fault; // true if cmd_vel timestamp and msg are old
+        bool dbw_ros_mode; // if it is 3 or 4
+        std::string mission_status; // expected values: GlobalPlanReady, MissionCancelled, MissionCompleted 
+        float tqL_cmd_controller; // left torque from velcontroller node
+        float tqR_cmd_controller; // right torque from velcontroller node
+        bool stop_ros; // stack flag to end mission
+        uint prevSt; // keep track of every previous state coming from transition, to know that we returning from fault 
+        uint delay; // having a 20 secs delay counter after returning to IDLE from fault
+        uint delay_threshold; // its update_frequency * (20secs)
+        allStates state; // containing enum of all expected states
+        double raptor_hb_timestamp; // collect timestamp of raptor msg
+        double cmdvel_timestamp; // initializes to ros time now for every new cmd_vel msg 
+        uint speed_state; // raptor ready to move state
+        uint au_state; // to publish std_msgs to stack
         double counter;
-        float cmdvel_timeout;
-        float raptorhb_timeout;
-        int update_freq;
-        float brake_disengaged_threshold;
+        float cmdvel_timeout; // allowable timeout
+        float raptorhb_timeout; //allowable threshold 
+        int update_freq; // time update freq
         
         // Publishers
         ros::Timer timer;
-        ros::Publisher pub_mobility;
+        ros::Publisher pub_mobility; 
         ros::Publisher pub_states;
 
         // Subscribers
