037bfd5cdb81c740826a7bd5321ea93bca1e1731
pavankumarathinarapu <athinak@clemson.edu>
2024-09-10 16:29:27 -0400

    Add periodic restart of candump in case it crashes

--- src/deeporange14_control/include/deeporange14_control/DataLogger.h
+++ src/deeporange14_control/include/deeporange14_control/DataLogger.h
@@ -25,25 +25,15 @@ namespace deeporange14
 
         private:
         void recordRosbagAndCANlog(const deeporange14_msgs::RaptorStateMsg::ConstPtr& msg);
-        void monitorFileSize(std::string &can_file, std::string &ros_bag);
-
-        ros::Timer timer_;
-
-        std::string pid_candump;
         
         // Subscriber object
         ros::Subscriber sub_raptor_;
-        int kill_timer;
-        int logging_counter;
-        double can_log_size;
-        double ros_bag_size;
-        double curr_can_log_size;
-        double curr_ros_bag_size;
-        std::string can_log_name;
-        std::string ros_bag_name;
 
         // Recording status (currently recording / not recording)
         bool isRecording;
+        bool isSystemFault;
+        ros::Time lastStartcandump;
+        ros::Duration startcandumpPeriod;
 
         std::string topic_ns = "/deeporange1314";
     };
--- src/deeporange14_control/src/DataLogger.cpp
+++ src/deeporange14_control/src/DataLogger.cpp
@@ -1,10 +1,10 @@
 /* 
     A class definition to automatically initiate Rosbag record (for certain
     topics) and CAN data logging (for all CAN buses together). Data logging 
-    starts (if not already happening) if the Raptor ECU is in a system state
-    from [SS2_WAKE, SS8_NOMINALOP]. The data logging (rosbag and CAN log) is
-    stopped if the system state becomes SS_31 or an error state (>= 200) is
-    reached.
+    starts (if not already happening) when the signal log_cmd in /raptor_state
+    (received via CAN) becomes 1 and stops when it becomes 0.
+    In addition, a (retro-active) CAN log file is created when the Raptor
+    system state becomes larger than or equal to 100 (which indicates a fault state)
 */
 
 #include <deeporange14_control/DataLogger.h>
@@ -14,101 +14,61 @@ namespace deeporange14
     DataLogger::DataLogger(ros::NodeHandle &node, ros::NodeHandle &priv_nh)
     {
         // Obtain ros::Subscriber object
-        sub_raptor_ = node.subscribe(topic_ns + std::string("/raptor_state"), 10, &DataLogger::recordRosbagAndCANlog, this, ros::TransportHints().tcpNoDelay(true));
+        sub_raptor_ = node.subscribe(std::string(topic_ns + "/raptor_state"), 10, 
+                                     &DataLogger::recordRosbagAndCANlog, this, 
+                                     ros::TransportHints().tcpNoDelay(true));
         
         // Initialize recording state to false
         isRecording = false;
+        isSystemFault = false;
 
-        kill_timer = 400; // at 50 Hz, 400 should be 8 seconds
-        logging_counter = 200;
+        // start candump
+        startcandumpPeriod = ros::Duration(5.0); // 5 seconds
+        lastStartcandump = ros::Time::now();
+        system("~/Scripts/startcandump");
     }
     
     DataLogger::~DataLogger(){}
 
+    /* callback for the topic /raptor_state */
     void DataLogger::recordRosbagAndCANlog(const deeporange14_msgs::RaptorStateMsg::ConstPtr& msg)
     {
-        if (!isRecording && msg->log_cmd)
+        // restart candump if it failed for some reason
+        ros::Time now = ros::Time::now();
+        if ((now - lastStartcandump) > startcandumpPeriod)
         {
-            ros::param::set("/log_status", 1);
-            ROS_INFO("Entered Data logger");
-            // Obtaining timestamp (EST) to name ROS bag and CAN dump
-            boost::posix_time::ptime my_posix_time = ros::Time::now().toBoost();
-            typedef boost::date_time::local_adjustor<boost::posix_time::ptime, -5, boost::posix_time::us_dst> us_eastern;
-            my_posix_time = us_eastern::utc_to_local(my_posix_time);    // Conversion from UTC to EST (Clemson, South Carolina)
-            std::string iso_time_str = boost::posix_time::to_iso_string(my_posix_time);
-            std::string file_name_header = std::string("do14_") + iso_time_str.substr(0,4) + std::string("-") + iso_time_str.substr(4,2) + std::string("-") + iso_time_str.substr(6,2) + 
-                        std::string("_") + iso_time_str.substr(9,2) + std::string("-") + iso_time_str.substr(11,2) + std::string("-") + iso_time_str.substr(13,2);
-            can_log_name = file_name_header + std::string(".CAN.log");
-            ros_bag_name = file_name_header + std::string(".ROS.bag");
-            std::string logs_dir = std::string("~/do14_logs/");
-            can_log_name = logs_dir + can_log_name;
-            ros_bag_name = logs_dir + ros_bag_name;
-            std::cout<<"ISO Time: "<<iso_time_str<<std::endl;
-            std::cout<<"CAN log name: "<<can_log_name<<std::endl;
-            std::cout<<"ROS bag name: "<<ros_bag_name<<std::endl;
+            lastStartcandump = now;
+            system("~/Scripts/startcandump");
+        }
 
-            // Record CAN log for all CAN buses combined
-            system(("candump any -ta >" + can_log_name + " &").c_str());
-            // Record ROS bags for relevant topics only
-            system(("rosbag record -e '(.*)cmd_mobility(.*)' -e '(.*)cmd_vel_reprojected(.*)' -e '(.*)cmd_trq(.*)' -e '(.*)pid_components(.*)' -e '(.*)remapping_state(.*)' -e '(.*)brake_command(.*)' -e '(.*)gps(.*)' -e '(.*)pose(.*)' -e '(.*)cmd_vel(.*)' -e '(.*)/novatel/oem7(.*)' -e '(.*)local_planner_and_controller(.*)' -e '(.*)tf(.*)' -x '(.*)approach_object(.*)' -x '(.*)approach_object_behavior(.*)' -x '(.*)global_costmap(.*)' -e '(.*)global_planner(.*)' -x '(.*)goto_object_behavior(.*)' -x '(.*)local_costmap(.*)' -x '(.*)omnigraph(.*)' -x '(.*)point_cloud_pipeline(.*)' -x '(.*)point_cloud_cache(.*)' -e '(.*)local_planner(.*)' -e '(.*)odom(.*)' -e '(.*)navigation_manager(.*)' -O " + ros_bag_name + " __name:=rosbag_recording &").c_str());
-            //system(("rosbag record -a -O " + ros_bag_name + " __name:=rosbag_recording &").c_str());
-
-            // Update recording state
+        // toggle logging if the value of log_cmd changes
+        if (msg->log_cmd && !isRecording)
+        {
+            // start both ROS and CAN logging -- runs in the background and terminates when logging is stopped
+            system("~/Scripts/startlogging &");
             isRecording = true;
-            
-            ROS_INFO("Started data recording");
-            //ROS_INFO("Current System State = %d", msg->system_state);
+            ROS_INFO("Started CAN and ROS logging. Current System State = %d", msg->system_state);
         }
-        else if (isRecording && !msg->log_cmd)
+        else if (!msg->log_cmd && isRecording)
         {
-            // Wait for 10 seconds before killing data logging to capture crucial data in case of system error
-            //ros::Duration(10).sleep();
-           
-            if (kill_timer > 0){
-                kill_timer--;
-                return;
-            }
-            else {
-                // Kill rosbag record and CAN logging
-                system("rosnode kill /rosbag_recording");
-                system("killall -SIGKILL candump");
-
-                // Update recording state
-                isRecording = false;
-                kill_timer = 400; // reinitialized for next recording
-                ROS_INFO("Rosbag record and CAN dump killed");
-                ros::param::set("/log_status", 0);
-            }
+            // stop both ROS and CAN logging
+            system("~/Scripts/stoplogging");
+            isRecording = false;
+            ROS_INFO("Rosbag record and CAN logging terminated");
         }
-        // this->monitorFileSize(can_log_name, ros_bag_name);
+        
+        // create an error log if the system state indicates a fault
+        if (msg->system_state > 100 && !isSystemFault)
+        {
+            // transition to fault state has occurred
+            system(("~/Scripts/savelogonerror "+std::to_string(msg->system_state)).c_str());
+            isSystemFault = true;
+            ROS_INFO("Creating error log.  Current System State = %d", msg->system_state);
+        }
+        else if (msg->system_state <=100)
+        {
+            // recovered from system fault --> reset state
+            isSystemFault = false;
+        }
     }
-
-    // void DataLogger::monitorFileSize(std::string &can_file, std::string &ros_bag) {
-    //         if (logging_counter == 100){
-    //             // Check for file size after every 2 second
-    //             can_log_size = system(("$stat -c %s " + can_file+ " &").c_str());
-    //             ros_bag_size = system(("$stat -c %s " + ros_bag + ".active"+ " &").c_str());
-    //         }
-    //         if (logging_counter == 0) {
-    //             // Check for file size again after every 4 second
-    //             curr_ros_bag_size = system(("$stat -c %s " + ros_bag + ".active"+ " &").c_str());
-    //             curr_can_log_size = system(("$stat -c %s " + can_file+ " &").c_str());
-
-    //             if(curr_ros_bag_size-ros_bag_size == 0) {
-    //                 ROS_WARN("ROS bag size not increasing. No new ROS data is being recorded!");  
-    //                 ros::param::set("/log_status", 0);              
-    //             }
-    //             if(curr_can_log_size-can_log_size == 0) {
-    //                 ROS_WARN("CAN log file size not increasing. No new CAN data is being recorded!");
-    //                 ros::param::set("/log_status", 0);
-    //             }
-    //             logging_counter = 200; // reinitialized for next recording
-    //         }
-    //         logging_counter--;
-    // }
 }
-
-
-
-
-
